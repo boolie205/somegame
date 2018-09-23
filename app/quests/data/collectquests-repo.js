1d15432c6ecf0762cce0f654753f8d06fdb039cd
Lewis <boolie205@gmail.com>
2018-09-23 16:43:22 +0000

    Quest system overhaul: better data flow

--- app/quests/data/collectquests-repo.js
+++ app/quests/data/collectquests-repo.js
@@ -1,9 +1,45 @@
 testquest = {
     "id": 1,
-    "name": "Find this  doggo",
-    "description": "bloke lost his doggo, help a brotha out",
+    "name": "Runaway Doggo",
+    "description": "As you're riding down the path you come accross a crying , rather pathetic looking man.",
+    "npctext": "Hey! Stanger! Please help, I was wondering these roads when I was waylayed by a band of angry kittens! I tried to fight them off with my dildo collection but in the battle my doggo was gravely injured and ran off into a near by cave, I'm too much of a pussy to get him myself but perhaps you, brave faggotknight will be brave enough to rescue my doggo? Hell we cant even afford him. SO you can keep him if you save him!",
     "reward": "Mangey doggo",
     "type": "collect"
+  };
+
+  anothertest = {
+    "id": 2,
+    "name": "The Black Knight",
+    "description": "The black knight wants to to find his watermelon and chicken.",
+    "npctext": "Yo dawg, I was out here tryina' save maidens n' sheet. Brother tryina smash real talk. So I save this hoe and ask where she from, of course bitch is from the other side of the realm. So I say she can stay with me for the night, rest up and I'll take her home. Wake up in the morning, and the bitch streight up took my watermelon and fried chicken and bolted. I'm finna kill that white ass hoe. Can you track her down and bring my chicken and watermelon back before I pop my gat at this hoe?",
+    "reward": "You can say the word Nigga, my G",
+    "type": "collect"
+  };
+
+  
+  questthethird = {
+    "id": 3,
+    "name": "Robins quest",
+    "description": "Robin wants some redbull",
+    "npctext": "Robin wants redbull, but only hads 5 SEK, can you get him some redbull?",
+    "reward": "Crippling Depression.",
+    "type": "collect",
+    "requiredamount": "1",
+    
+  };
+
+  
+  
+  demonquest = {
+    "id": 4,
+    "name": "Kill them all",
+    "description": "siewl wants you to kill 5 demons" ,
+    "npctext": "Lorum ipsum dolor sit amet, kill demon bacom pigfat phone kek rofl",
+    "reward": "Silverlight",
+    "type": "kill",
+    "requiredamount": "5",
+    "requiredentity": "mobID"
+    
   };
 
 
--- app/quests/questlogic/collectquests.js
+++ app/quests/questlogic/collectquests.js
@@ -1,17 +1,15 @@
-//This is going to actually hold all the logic for this quest type.
-// Not sure when I should run this function, I guess i COULD do it onclick of the quest NPC. run the check then display "success text"
-
-let questID = 1;  // Gunna gave to read this from the "context menu" once in game, IE player clicks on NPC, then selects quest. THEN this function is run.
-let questComplete = false; // also the reason this is here rather than being declared inside the function is because I need it to be global scope. Spose I could use return tho but fuck you
-let questInfo = []; // gunna have an array passed into this from the collects quest repo
-
-function isComplete(questID, questInfo ){
-if (amountNeeded === amountHeld){
-      questComplete = true; 
-
-}else {
-  //fuck all atm mate
-}
-}; 
+$(document).ready(function() {
+  var questtitle = document.getElementById("questtitle");
+  var questsummery = document.getElementById("questsummery");
+  var questtext = document.getElementById("questtext");
+  var questContext = questthethird;
 
 
+  questtitle.innerHTML = questContext.name;
+  questsummery.innerHTML = questContext.description;
+  npctext.innerHTML = questContext.npctext;
+  questtext.innerHTML = "You will recieve" + " " + questContext.reward;
+ 
+
+
+});
